0019c089de0f9811de8faa22f1983c0d964dad32
Ryan Duryea <aguynamedryan@gmail.com>
2014-12-04 16:51:45 -0800

    scrub_files: update for newest set of photos to ignore

--- scrub_files.rb
+++ scrub_files.rb
@@ -1,4 +1,32 @@
 # This script erases a handful of photos that I didn't want to include in the final project
-%w(12 22 30 34 41 42 82 104 114 125).map(&:to_i).map { |i| sprintf('"%04d*"', i) }.each do |pattern|
+%w(
+42
+60
+84
+85
+86
+97
+122
+131
+146
+187
+192
+194
+202
+203
+204
+205
+206
+207
+208
+209
+210
+211
+212
+213
+220
+225
+232
+).map(&:to_i).map { |i| sprintf('"%04d*"', i) }.each do |pattern|
   `find . -name #{pattern} | xargs rm`
 end
